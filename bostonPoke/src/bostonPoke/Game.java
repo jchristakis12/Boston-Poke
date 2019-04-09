31bb528e8be64598f31272f19c88347e75978cb8
Men <mens@wit.edu>
2019-04-09 13:28:14 -0400

    New battle scene

--- bostonPoke/src/bostonPoke/Game.java
+++ bostonPoke/src/bostonPoke/Game.java
@@ -3,28 +3,17 @@ package bostonPoke;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class Game extends Application implements Display {
+public class Game extends Application{
 	
-	
-	public static final int Width = 640, Height = Width / 12 * 9;
-	public Thread thread;
-	private boolean running = false;
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		new Game();
-	}
-	
-	public Game() {
-		new Window(Width, Height, "Boston Poke", this);
+		Map.main(args);
 	}
 
 	@Override
 	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
+		
 		
 	}
-	
 }
 	
 
