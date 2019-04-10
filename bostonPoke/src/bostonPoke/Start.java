e3fc43c9db668881f38646b7f4564bca7b5860c5
Guarino <guarinof@wit.edu>
2019-04-10 12:44:26 -0400

    The start screen

---
+++ bostonPoke/src/bostonPoke/Start.java
@@ -0,0 +1,54 @@
+package bostonPoke;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Application;
+import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Start extends Application {
+
+	
+	public static GridPane start() throws Exception {
+		// TODO Auto-generated method stub
+		
+		GridPane p = new GridPane();
+		Text Welcome =  new Text("Welcome");
+		p.add(Welcome, 100, 0);
+		
+		Text Name = new Text("Name:");
+		p.add(Name, 2, 2);
+		TextField enterName = new TextField(); 
+		p.add(enterName, 3, 2);
+		
+		Text Gender = new Text("Gender:");
+		p.add(Gender, 2, 4);
+		
+		TextField enterGender = new TextField();
+		p.add(enterGender, 3, 4);
+		
+		
+		
+		
+		
+		
+		return p;
+		
+	}
+
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
