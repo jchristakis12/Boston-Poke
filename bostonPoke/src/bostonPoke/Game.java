5ff374dd01e69a1c349763cfc72c82831cb092ed
Christakis <christakisj@wit.edu>
2019-04-12 05:11:40 -0400

    Start screen updates

--- bostonPoke/src/bostonPoke/Game.java
+++ bostonPoke/src/bostonPoke/Game.java
@@ -3,6 +3,7 @@ package bostonPoke;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.event.EventHandler;
@@ -17,6 +18,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -30,24 +32,24 @@ import monsters.Monster;
 
 
 public class Game extends Application{
-	
-	static String playerName = null;
-	static String playerGender = null;
-	static Monster playerMonster;
-	
-	static Trainer Womusk;
-	static Trainer bigChungus;
 
-	public static void main(String[] args) {
+static String playerName;
+static String playerGender;
+static Monster playerMonster;
 
-		
-		
+static Trainer Womusk;
+static Trainer bigChungus;
+
+public static void main(String[] args) {
+
+ 
+ 
 		Trainer Womusk = new Trainer(2, false);
 		HomelessMan myDude = new HomelessMan(); 
 		Geese Fronto = new Geese();
 		Womusk.setMonster(myDude, 0);
 		Womusk.setMonster(Fronto, 1);
-		
+ 
 		Trainer bigChungus = new Trainer(3, true);
 		GreenLine blueLine = new GreenLine();
 		GreenLine commuterRail = new GreenLine();
@@ -55,31 +57,31 @@ public class Game extends Application{
 		bigChungus.setMonster(blueLine, 0);
 		bigChungus.setMonster(meatShield, 1);
 		bigChungus.setMonster(commuterRail, 2);
-		
+ 
 		Player player = new Player(playerName, playerGender);
-		
-		
-		
+ 
+ 
+ 
 		launch(args);
-		
+ 
 		System.out.println(playerName);
-	}		
+}  
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		
+@Override
+public void start(Stage primaryStage) throws Exception {
+ 
 		BorderPane map = Map1.getMap();
 		Pane pane2 = new Pane();
-		FlowPane startS = Start.start();
-		
+		Pane startS = Start.start();
+ 
 		Image boss1 = new Image("http://www.stickpng.com/assets/images/58b1702a102ddecdee0dd039.png");
 		ImageView imgView8 = new ImageView(boss1);
-		imgView8.setLayoutX(425);
+		imgView8.setLayoutX(375);
 		imgView8.setLayoutY(100);
-		imgView8.setFitHeight(100);
-		imgView8.setFitWidth(100);
-				
-		
+		imgView8.setFitHeight(125);
+		imgView8.setFitWidth(175);
+   
+ 
 		//Creates the character image and inserts it into the map and handles user input to move the image around 
 		Image character = new Image("https://vignette.wikia.nocookie.net/pokemon/images/7/7a/VS_Red_SM.png/revision/latest?cb=20170101032644");
 		ImageView mainChar = new ImageView(character);
@@ -87,88 +89,130 @@ public class Game extends Application{
 		mainChar.setLayoutY(750);
 		mainChar.setFitHeight(70);
 		mainChar.setFitWidth(100);
-				
+   
 		mainChar.setOnKeyPressed(new EventHandler<KeyEvent>() {
-		
-			
-			@Override
-			public void handle(KeyEvent event) {
-				// TODO Auto-generated method stub
-				if (event.getCode() == KeyCode.LEFT) {
-					mainChar.setX(mainChar.getX() - 10);
-				}
-				if (event.getCode() == KeyCode.RIGHT) {
-					mainChar.setX(mainChar.getX() + 10);
-				}
-				if (event.getCode() == KeyCode.UP) {
-					mainChar.setY(mainChar.getY() - 10);
-				}
-				if (event.getCode() == KeyCode.DOWN) {
-					mainChar.setY(mainChar.getY() + 10);
-				}
+ 
+  
+		@Override
+		public void handle(KeyEvent event) {
+		// TODO Auto-generated method stub
+			if (event.getCode() == KeyCode.LEFT) {
+				mainChar.setX(mainChar.getX() - 10);
 			}
-		}); 
+			if (event.getCode() == KeyCode.RIGHT) {
+				mainChar.setX(mainChar.getX() + 10);
+			}
+			if (event.getCode() == KeyCode.UP) {
+				mainChar.setY(mainChar.getY() - 10);
+			}
+			if (event.getCode() == KeyCode.DOWN) {
+				mainChar.setY(mainChar.getY() + 10);
+			}
+  	}
+ }); 
 		map.getChildren().addAll(mainChar,imgView8);
 		Scene mainMap = new Scene(map, 1000,800);
-		
+ 
 		//Creates the scene for the first battle and handles 
 		final FXMLLoader loader = new FXMLLoader(getClass().getResource("Battle.fxml"));
 		Pane battleScene1 = loader.load();
 		Scene battle1 = new Scene(battleScene1,1000,800);
-		Scene start = new Scene(startS, 1000, 800);
+		Scene start = new Scene(startS, 500, 500);
+		
 
 		primaryStage.setTitle("Start Game!");
 		primaryStage.setScene(start);
 		primaryStage.show();
 
 		mainChar.requestFocus();
+ 
 
-		Button submit = new Button("SUBMIT");
-		startS.getChildren().add(submit);
-		
-		TextField enterName = new TextField(); 
-		((FlowPane) startS).getChildren().add(enterName);
-		TextField Gender = new TextField();
-		((FlowPane) startS).getChildren().add(Gender);
+		TextField enterName = new TextField();
+		//enterName.setPrefSize(100, 30);
+		((GridPane) startS).add(enterName, 1, 1, 3, 1);
+		TextField enterGender = new TextField();
+		//enterGender.setPrefSize(100, 30);
+		((GridPane) startS).add(enterGender, 1, 2, 3, 1);
 
-		
+ 
 		Button squirrel = new Button("Squirrel");
-		((GridPane) startS).add(squirrel, 2, 8);
+		//squirrel.setAlignment(Pos.CENTER);
+		//squirrel.setPrefSize(75, 25);
+		((GridPane) startS).add(squirrel, 1, 3);
 		
+		Button geese = new Button("Geese");
+		//geese.setPrefSize(75, 25);
+		geese.setAlignment(Pos.CENTER);
+		((GridPane) startS).add(geese, 2, 3);
+		
+		Button homelessman = new Button("Homeless man");
+		//homelessman.setPrefSize(75, 25);
+		homelessman.setAlignment(Pos.CENTER);
+		((GridPane) startS).add(homelessman, 3, 3);
+		
+		
+ 
 		squirrel.setOnAction(e->{
-			playerMonster = Player.selectMonster(1);
-			if(playerMonster instanceof Monster) {
-				System.out.print(playerName);
-			}
+		playerMonster = Player.selectMonster(1);
+		playerName = enterName.getText();
+		playerGender = enterGender.getText();
+		if(playerMonster instanceof Monster) {
+			System.out.println(playerName);
+			System.out.println(playerMonster.getName());
+			System.out.println(playerGender);
+		}
+		primaryStage.setTitle("MAIN MAP");
+		primaryStage.setScene(mainMap);
+
 		});
 		
-		submit.setOnAction(e->{
-			//String playerName = startS how to access textfield from game
+		geese.setOnAction(e->{
+			playerMonster = Player.selectMonster(1);
 			playerName = enterName.getText();
-			playerGender = Gender.getText();
+			playerGender = enterGender.getText();
+			if(playerMonster instanceof Monster) {
+				System.out.println(playerName);
+				System.out.println(playerMonster.getName());
+				System.out.println(playerGender);
+			}
 			primaryStage.setTitle("MAIN MAP");
 			primaryStage.setScene(mainMap);
-		});
+
+			});
 		
-		
-		
+		homelessman.setOnAction(e->{
+			playerMonster = Player.selectMonster(1);
+			playerName = enterName.getText();
+			playerGender = enterGender.getText();
+			if(playerMonster instanceof Monster) {
+				System.out.println(playerName);
+				System.out.println(playerMonster.getName());
+				System.out.println(playerGender);
+			}
+			primaryStage.setTitle("MAIN MAP");
+			primaryStage.setScene(mainMap);
+
+			});
+ 
+ 
 		imgView8.setOnMouseClicked(e->{
-			primaryStage.setTitle("Fight!");
-			primaryStage.setScene(battle1);
-		});
-		
+		primaryStage.setTitle("Fight!");
+		primaryStage.setScene(battle1);
+ });
+ 
 
 
 
 		mainChar.requestFocus();
 
-		
-	};
-		
-		
+ 
+};
+ 
+ 
 
 }
-	
+
+
 
 
 
--- bostonPoke/src/bostonPoke/Start.java
+++ bostonPoke/src/bostonPoke/Start.java
@@ -5,6 +5,7 @@ import java.util.ResourceBundle;
 
 import javafx.application.Application;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -20,62 +21,30 @@ import javafx.stage.Stage;
 public class Start extends Application {
 
 	
-	public static FlowPane start() throws Exception {
+	public static GridPane start() throws Exception {
 		// TODO Auto-generated method stub
 		
-<<<<<<< HEAD
-		FlowPane p = new FlowPane();
-		p.setVgap(20);
-		p.setHgap(20);
-=======
-		
-		
-		
-		
-		
-		
-		
-		
-		GridPane p = new GridPane();
-		Text Welcome =  new Text("Welcome");
-		p.add(Welcome, 100, 0);
->>>>>>> f84868d68da430c7999f9240980cbad6e8df21db
-		
-		
-		HBox hbox = new HBox(10);
-		hbox.setPrefSize(200, 200);
-		
-		VBox vbox = new VBox(20);
-		vbox.setPrefSize(200, 200);
-		
-		VBox vbox1 = new VBox(20);
-		vbox1.setPrefSize(200, 200);
-		vbox1.setLayoutX(200);
-		vbox1.setLayoutY(200);
-		
-		Text Welcome =  new Text("Welcome");
-		Welcome.setFont(Font.font("Verdana", 45));
-		
-		
-		
-		
-		Text Name = new Text("Name:");
-		Name.setFont(Font.font("Verdana", 34));
-		
-		
-		Text Gender = new Text("Gender:");
-		Gender.setFont(Font.font("Verdana", 34));
-		
-		hbox.getChildren().add(Welcome); 
-		vbox.getChildren().addAll(Name, Gender);
-		
-		vbox1.getChildren().addAll(hbox, vbox);
-		
-		p.getChildren().addAll(vbox1);
 
+		GridPane p = new GridPane();
+		//p.setGridLinesVisible(true);
+		p.setAlignment(Pos.CENTER);
+		p.setPrefSize(500, 400);
+		p.setHgap(10);
+		p.setVgap(10);
+		p.setPadding(new Insets(5));
+		Text Welcome =  new Text("Welcome");
+		Welcome.setFont(Font.font("Tahoma", 32));
 		
+		p.add(Welcome, 0, 0, 3, 1);
+
+		Text Name = new Text("Name: ");
+		p.add(Name, 0, 1);
 		
+		Text Gender = new Text("Gender: ");
+		p.add(Gender, 0, 2);
 		
+		Text SelectPokemon = new Text("Select Pokemon: ");
+		p.add(SelectPokemon, 0, 3);
 		
 		return p;
 		
